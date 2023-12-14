c2cc4796feec9aeef7baa1923f536bcd24c00c68
Danrlei <danrleidalfre@gmail.com>
2023-12-14 17:02:42 -0300

    implement Header app

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { Layout } from './layouts/Layout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
