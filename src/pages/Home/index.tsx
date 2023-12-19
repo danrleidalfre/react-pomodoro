384a7f58befe373a6a403f31bd4fc39452d7edcd
Danrlei <danrleidalfre@gmail.com>
2023-12-19 14:27:37 -0300

    implement validation form with react hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,17 +8,49 @@ import {
   Separator,
   StartCountDownButton,
 } from './styles.ts'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { string } from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: string().min(1, 'Informe um nome para a tarefa'),
+  minutes: zod
+    .number()
+    .min(5, 'o cíclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'o cíclo precisa ser de no máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutes: 25,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const minutes = watch('minutes')
+  const isButtonDisabled = !task || !minutes
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <InputTask
             id="task"
             list="suggestions"
             placeholder="Dê um nome para seu projeto"
+            {...register('task')}
           />
           <datalist id="suggestions">
             <option value="Estudar" />
@@ -33,6 +65,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutes', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -46,7 +79,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDownButton type="submit">
+        <StartCountDownButton disabled={isButtonDisabled} type="submit">
           <Play />
           Começar
         </StartCountDownButton>
